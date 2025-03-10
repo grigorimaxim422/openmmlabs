b2232dff17652f19c35975053393c2e4baa737ee
grigorimaxim422 <grigorimaxim422@gmail.com>
2025-03-10 09:36:12 +0000

    fixing

--- benchmark.sh
+++ benchmark.sh
@@ -1,4 +1,6 @@
 # python3 -m tests.benchmark
 # python3 -m tests.benchmark --platform=CPU --seconds=10 --test=pme
 #python benchmark.py --platform=CUDA --style=table --test=pme,apoa1pme,amber20-cellulose,amber20-stmv,amoebapme
-python3 -m  tests.benchmark --style=table --platform=CUDA  --test=pme,apoa1pme,amber20-cellulose,amber20-stmv,amoebapme
+
+python3 -m  tests.benchmark --style=table --platform=CPU --seconds=10  --test=pme,apoa1pme,amber20-cellulose,amber20-stmv,amoebapme
+# python  -m  tests.benchmark --style=table --platform=CPU --seconds=10 --test=gbsa,rf,apoa1rf,apoa1ljpme
